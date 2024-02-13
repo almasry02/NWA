9d884e69113fab1851efd4180b657d82b5321c7a
emiltrz <220478@studierende.htl-donaustadt.at>
2024-02-13 09:00:30 +0100

    commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './sections/header/header.component';
+import { FooterComponent } from './sections/footer/footer.component';
+import { TeacherComponent } from './pages/teacher/teacher.component';
+import { StudentsComponent } from './pages/students/students.component';
+import { LoginComponent } from './pages/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TeacherComponent,
+    StudentsComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
